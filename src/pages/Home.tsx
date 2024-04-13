ada94a88383ec7f3d92869d724d76f2156618c23
dgshea <dgshea@ncsu.edu>
2024-04-13 19:04:53 -0400

    added local routing - need to use hash routing for ghpages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div className="Home">
+          <h1>
+            Rebecca Daggerhart's website <i>coming soon...</i>
+          </h1>
+      </div>
+    );
+};
+
+export default Home;
